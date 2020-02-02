2333cf3062410e19472bc23dee38ef962eaa63b4
yg <wjd0r@naver.com>
2020-02-02 15:39:09 +0900

    add navigation bar

---
+++ src/components/AppShell.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: 'auto',
+    },
+}
+
+class AppShell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: false
+        };
+    }
+    handelDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handelDrawerToggle}>
+                        <MenuIcon/>
+                    </IconButton>
+                </AppBar>
+                <Drawer open={this.state.toggle}>
+                    <MenuItem onClick={this.handelDrawerToggle}>Home</MenuItem>
+                    <MenuItem onClick={this.handelDrawerToggle}>Home 2</MenuItem>
+                </Drawer>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(AppShell);
